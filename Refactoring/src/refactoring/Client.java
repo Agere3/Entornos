8c742f0cff66017ce1cb886e6088eb8b785007dc
Agere3 <cf20eric.rubio@iesjoandaustria.org>
2021-04-22 16:07:20 +0200

    hasta el 7

--- Refactoring/src/refactoring/Client.java
+++ Refactoring/src/refactoring/Client.java
@@ -66,7 +66,7 @@ public class Client {
                     break;
             }
             */
-        	double quantitat = quantitatPerLloguer(lloguer); //nueva linea
+        	double quantitat = lloguer.quantitat(); //nueva linea
         
 
             // afegeix lloguers freqüents
@@ -93,26 +93,6 @@ public class Client {
         return resultat;
     }
 
-	private double quantitatPerLloguer(Lloguer lloguer) {
-		double quantitat = 0;
-        switch (lloguer.getVehicle().getCategoria()) {
-            case Vehicle.BASIC:
-                quantitat += 3;
-                if (lloguer.getDies() > 3) {
-                    quantitat += (lloguer.getDies() - 3) * 1.5;
-                }
-                break;
-            case Vehicle.GENERAL:
-                quantitat += 4;
-                if (lloguer.getDies() > 2) {
-                    quantitat += (lloguer.getDies() - 2) * 2.5;
-                }
-                break;
-            case Vehicle.LUXE:
-                quantitat += lloguer.getDies() * 6;
-                break;
-        }
-		return quantitat;
-	}
+	
 }
 
